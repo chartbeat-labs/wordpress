609736ebef04ebfb3462f8d8a736cd33863f7b3e
Chris Breaux <christopher@chartbeat.com>
2017-01-31 17:20:34 -0500

    Remove deprecated widget

--- chartbeat.php
+++ chartbeat.php
@@ -117,7 +117,7 @@ function chartbeat_options_page() {
 						No
 					</td>
 				</tr>
-	
+
 				<tr>
 					<th scope="row"><?php _e('Enable Chartbeat Publishing?','chartbeat'); ?><br /> <small>Sign
 							up for <a href="http://chartbeat.com/publishing/">Chartbeat Publishing</a>.
@@ -154,91 +154,8 @@ function chartbeat_options_page() {
 					<td><input size="30" type="text" name="chartbeat_apikey" value="<?php echo esc_attr( get_option('chartbeat_apikey') ); ?>" />
 					</td>
 				</tr>
-	
 			</table>
 			<br /> <br />
-
-			<script src="<?php echo plugins_url('media/topwidget.compiled.js', __FILE__); ?>" type="text/javascript"></script>
-			<script type="text/javascript"> 
-			var themes = { 'doe':   { 'bgcolor': '', 'border': '#dde7d4', 'text': '#555' },
-				'gray':  { 'bgcolor': '#e3e3e3', 'border': '#333333', 'text': '#555', 'header_bgcolor': '#999999', 'header_color': '#fff' },
-				'red':   { 'bgcolor': '#ffffff', 'border': '#cc3300', 'text': '#555', 'header_bgcolor': '#f5c5be', 'header_color': '#fff' },
-				'blue':  { 'bgcolor': '#e0ecff', 'border': '#3a5db0' },
-				'green': { 'bgcolor': '#c9edcc', 'border': '#69c17d', 'text': '#555' } };
-			var theme = 'doe';
-			var limit = 10;
-
-			function changeTheme(select) {
-				theme = select.options[select.selectedIndex].value;
-				renderWidget();
-			}
-			
-			function changeLimit(select) {
-				limit = select.options[select.selectedIndex].value;
-				renderWidget();
-			}
-			
-			function renderWidget() {
-				new CBTopPagesWidget( '<?php echo esc_js(get_option('chartbeat_apikey')) ?>',
-								   { 'host': '<?php echo esc_js( chartbeat_get_display_url( $domain ) ); ?>',
-									 'background': themes[theme]['bgcolor'],
-									 'border': themes[theme]['border'],
-									 'header_bgcolor': themes[theme]['header_bgcolor'],
-									 'header_color': themes[theme]['header_color'],
-									 'text': themes[theme]['text'],
-									 'limit': limit });
-			}
-
-			function addOption(array, key, val) {
-				array.push("'" + key + "': '" + val + "'");
-			}
-			function buildOptions() {
-				var options = [];
-				addOption(options, 'background', themes[theme]['bgcolor']);
-				addOption(options, 'border', themes[theme]['border']);
-				addOption(options, 'header_bgcolor', themes[theme]['header_bgcolor']);
-				addOption(options, 'header_color', themes[theme]['header_color']);
-				addOption(options, 'text', themes[theme]['text']);
-				addOption(options, 'limit', limit);
-				options = '{' + options.join(',') + '}';
-				document.getElementById('chartbeat_widgetconfig').value = options;
-				console.debug("options:" + options);
-			}
-			renderWidget();
-			</script>
-
-			If your theme supports it, you can also add a widget under
-			<tt>Appearance > Widgets</tt>
-			to show where users currently are on your site. <br> <br>
-			<table cellspacing="10">
-				<tr>
-					<td valign="top">Number of pages to show
-						<select name="metric" id="toplimit" onChange="changeLimit(this);">
-							<option value="5">5</option>
-							<option value="10" selected="selected">10</option>
-							<option value="20">20</option>
-							<option value="30">30</option>
-						</select>
-						<br /> <br />
-
-						Color scheme
-						<select name="theme" id="toptheme" onChange="changeTheme(this);">
-							<option value="doe">John Doe</option>
-							<option value="gray">Dorian Gray</option>
-							<option value="red">Red Rum</option>
-							<option value="blue">Blue Moon</option>
-							<option value="green">Green Giant</option>
-						</select>
-					</td>
-					<td>&nbsp;</td>
-					<td>
-						Sample:
-						<br> <br>
-						<div id="cb_top_pages"></div>
-					</td>
-				</tr>
-			</table>
-			<input type="hidden" id="chartbeat_widgetconfig" name="chartbeat_widgetconfig" value="{}" />
 
 			<p class="submit">
 				<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
@@ -252,26 +169,17 @@ function chartbeat_options_page() {
 function chartbeat_register_settings() {
 	register_setting('chartbeat-options','chartbeat_userid', 'intval');
 	register_setting('chartbeat-options','chartbeat_apikey','chartbeat_is_validmd5');
-	register_setting('chartbeat-options','chartbeat_widgetconfig','chartbeat_is_validjson');
 	register_setting('chartbeat-options','chartbeat_trackadmins','intval'); // add trackadmin setting
 	register_setting('chartbeat-options','chartbeat_enable_newsbeat','intval');
 	register_setting('chartbeat-options','chartbeat_enable_headline_testing','intval');
 }
 
 function chartbeat_is_validmd5($md5) {
-	if( !empty($md5) && preg_match('/^[a-f0-9]{32}$/', $md5) ) { 
+	if( !empty($md5) && preg_match('/^[a-f0-9]{32}$/', $md5) ) {
 		return $md5;
 	} else {
 		add_settings_error( 'chartbeat_apikey','invalid_apikey','API Key is not correct, please check again','error');
 		return 777;
-	}
-}
-
-function chartbeat_is_validjson($json_str) {
-	if( is_null(json_decode($json_str)) ) { 
-		return $json_str; 
-	} else {
-		add_settings_error( 'chartbeat_widgetconfig','invalid_widgetconfig','Widget config is malformed','error');
 	}
 }
 
@@ -289,10 +197,10 @@ function chartbeat_configs() {
 	if (is_single()) {
 		$post = get_queried_object();
 
-		// Use the author's display name 
+		// Use the author's display name
 		$author = get_the_author_meta('display_name', $post->post_author);
 		$cb_configs['author'] = apply_filters( 'chartbeat_config_author', $author );
-	
+
 		// Use the post's categories as sections
 		$cats = get_the_terms($post->ID, 'category');
 		if ($cats) {
@@ -333,7 +241,7 @@ function add_chartbeat_head() {
 }
 
 function add_chartbeat_config(){
-		
+
 		$cb_configs = chartbeat_configs();
 		if( 1 != get_option( 'chartbeat_enable_headline_testing' ) ) :
 		?>
@@ -358,7 +266,7 @@ function add_chartbeat_footer() {
 		// if visitor is admin AND tracking is off, do not load chartbeat
 		if ( current_user_can( 'manage_options') && get_option('chartbeat_trackadmins') == 0)
 			return $analytics ;
-		
+
 		?>
 		<script type="text/javascript">
 			<?php
@@ -395,7 +303,7 @@ function chartbeat_amp_add_analytics( $analytics ) {
 		if ( current_user_can( 'manage_options') && get_option('chartbeat_trackadmins') == 0)
 			return $analytics ;
 
-		$cb_configs = chartbeat_configs(); 
+		$cb_configs = chartbeat_configs();
 
 		$analytics['chartbeat'] = array(
 	    'type' => 'chartbeat',
@@ -409,7 +317,7 @@ function chartbeat_amp_add_analytics( $analytics ) {
 	  );
 
 	  $enable_newsbeat = get_option('chartbeat_enable_newsbeat');
-		if ($enable_newsbeat) { 
+		if ($enable_newsbeat) {
 			$analytics['chartbeat']['config_data']['vars']['authors'] = esc_js($cb_configs['author']);
 			$analytics['chartbeat']['config_data']['vars']['sections'] = esc_js($cb_configs['sections']);
 		}
@@ -441,37 +349,6 @@ function chartbeat_fbia_analytics( $analytics ) {
 
 add_action( 'instant_articles_article_header', 'chartbeat_fbia_analytics' );
 
-class Chartbeat_Widget extends WP_Widget {
-
-	function __construct() {
-        parent::__construct('chartbeat_widget', 'Chartbeat Widget',array( 'description' => __('Display your site\'s top pages')));
-        
-        if ( is_active_widget(false,false,$this->id_base,true) || is_admin() ) {
-        	wp_enqueue_script( 'chartbeat_topwidget', plugins_url('media/topwidget.compiled.js', __FILE__) );
-        	wp_localize_script( 'chartbeat_topwidget', 'cbproxy', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ),
-        		'cbnonce' => wp_create_nonce( 'cbproxy-nonce' ) ) );
-        }
-    }
-
-	function widget( $args, $instance ) {
-		extract( $args );
-		echo $before_widget;
-		
-		$api_key = get_option( 'chartbeat_apikey' );
-		$widget_config = get_option('chartbeat_widgetconfig');
-		
-		if ( $api_key ) : ?>
-		<div id="cb_top_pages"></div>
-		<script type="text/javascript">
-		var options = { };
-		new CBTopPagesWidget( <?php echo json_encode(get_option('chartbeat_widgetconfig')); ?> );
-		</script>
-		<?php
-		endif;
-		echo $after_widget;
-	}
-}
-
 // Add proxy for Chartbeat API requests
 add_action( 'wp_ajax_nopriv_cbproxy-submit', 'cbproxy_submit' );
 add_action( 'wp_ajax_cbproxy-submit', 'cbproxy_submit' );
@@ -496,109 +373,13 @@ function cbproxy_submit() {
 		}
 		set_transient($transient,$response,5);
 	}
-	
+
 	echo htmlspecialchars_decode( esc_js($response) );
 	exit;
 }
 
-// Dashboard Widget
-function chartbeat_widget_init() {
-	register_widget( 'Chartbeat_Widget' );
-}
-
-add_action('widgets_init', 'chartbeat_widget_init');
-
 function chartbeat_get_display_url( $url ){
 	return strtok(preg_replace("/(https?:\/\/)?(www\.)?/i","",$url),"/");
-}
-
-function chartbeat_dashboard_widget_function() {
-	?>
-	<div id="chartbeatGauge"></div>
-	<div id="chartbeatRefsTable" class="chartbeatWidget">
-		<table id="chartbeatLinks" class="chartbeatTable">
-			<thead>
-				<tr>
-					<th colspan=2 class="chartbeatLabel">Top Referrers</th>
-				</tr>
-			</thead>
-			<tbody></tbody>
-		</table>
-		<table id="chartbeatSearch" class="chartbeatTable">
-			<thead>
-				<tr>
-					<th colspan=2 class="chartbeatLabel">Top Search</th>
-				</tr>
-			</thead>
-			<tbody></tbody>
-		</table>
-		<div class="clear"></div>
-	</div>
-	<div id="chartbeatGraph" class="chartbeatWidget clear">
-		<div class="chartbeatLabel">Visits - Last 3 Day</div>
-		<div id="chartbeatHist">
-			<div id="annotations"></div>
-		</div>
-	</div>
-	<script type="text/javascript">
-	<?php add_filter( 'posts_where', 'chartbeat_filter_where_last_three_days' ); ?>
-	var events = [];
-	// Get published post Events 
-	<?php
-	$args = array( 'post_type' => array( 'post' ),'post_status' => 'publish', 'orderby' => 'date', 'order' => 'ASC' );
-	$the_query = new WP_Query( $args );
-	$domain = apply_filters( 'chartbeat_config_domain', chartbeat_get_display_url (get_option('home')) );
-	while ( $the_query->have_posts() ) : $the_query->the_post(); 
-		$tstamp = get_the_time('Y,n-1,j,G,i');
-		if ($tstamps[$tstamp])
-			continue; 
-
-		$tstamps[$tstamp] = true;
-		$category = get_the_category();
-		if($category[0])
-			$category_link = get_category_link($category[0]->cat_ID );
-
-		?>var ev = {domain:'<?php echo esc_js( chartbeat_get_display_url( $domain ) );?>',title:'<?php echo esc_js( get_the_title() ); ?>',
-	  	value:'<?php echo esc_js( chartbeat_get_display_url( $category_link ) ); ?>', group_name:'<?php echo esc_js( chartbeat_get_display_url( get_page_link() ) ); ?>',
-	  	t: new Date(<?php echo the_time('Y,n-1,j,G,i'); ?>).getTime()/1000,group_type:'page',num_referrers:10,id:'<?php echo esc_js( get_the_ID() ); ?>',type:'wp',data:{action_type:"create"}};
-		events.push(ev);
-	<?php
-	endwhile;
-	wp_reset_postdata();?>
-
-	// Get revisions
-	<?php
-	$args = array( 'post_type' => array( 'revision' ), 'post_status' => 'inherit', 'orderby' => 'date', 'order' => 'ASC' );
-	$the_query = new WP_Query( $args );
-	while( $the_query->have_posts() ) : $the_query->the_post();
-		$tstamp = get_the_time('Y,n-1,j,G,i');
-		if ($tstamps[$tstamp])
-			continue;
-
-		$tstamps[$tstamp] = true;
-		$category = get_the_category();
-		if($category[0])
-			$category_link = get_category_link($category[0]->cat_ID );
-
-		?>var ev = {domain:'<?php echo esc_js( chartbeat_get_display_url( $domain ) ); ?>',title:'<?php echo esc_js( get_the_title() ); ?>',
-	  	value:'<?php echo esc_js( chartbeat_get_display_url( $category_link ) ); ?>',group_name:'<?php echo esc_js( chartbeat_get_display_url( get_page_link() ) ); ?>',
-	  	t: new Date(<?php echo the_time('Y,n-1,j,G,i'); ?>).getTime()/1000,group_type:'page',num_referrers:10,id:'<?php echo esc_js( get_the_ID() ); ?>',type:'wp',data:{action_type:"update"}};
-		events.push(ev);
-	<?php
-	endwhile; 
-	wp_reset_postdata();
-
-	remove_filter( 'posts_where', 'chartbeat_filter_where_last_three_days' );
-	?>
-
-	function loadChartBeatWidgets(){
-		new CBDashboard('chartbeatGauge','chartbeatRefsTable','chartbeatHist',200,"<?php echo esc_js( chartbeat_get_display_url( $domain ) ); ?>","<?php echo esc_js( get_option('chartbeat_apikey') ); ?>",events);
-	};
-	
-	var currOnload = window.onload;
-	window.onload = (typeof window.onload != 'function') ? loadChartBeatWidgets : function() { oldonload(); loadChartBeatWidgets(); };
-	</script>
-	<?php
 }
 
 // Create a new filtering function that will add our where clause to the query
@@ -607,25 +388,6 @@ function chartbeat_filter_where_last_three_days( $where = '' ) {
 	$where .= $wpdb->prepare( " AND $wpdb->posts.post_modified > %s", date( 'Y-m-d', strtotime( '-3 days' ) ) );
 	return $where;
 }
-
-function chartbeat_add_dashboard_widgets() {
-	// Don't add widgets if we haven't set up Chartbeat yet
-	if ( ! get_option('chartbeat_userid') )
-		return;
-
-	wp_enqueue_style( 'cbplugin_css' );
-	wp_enqueue_script( 'cbdashboard' );
-	wp_add_dashboard_widget('chartbeat_dashboard_widget', 'Chartbeat', 'chartbeat_dashboard_widget_function');
-}
-
-function chartbeat_plugin_admin_init() {
-	wp_register_style('cbplugin_css',plugins_url('media/cb_plugin.css', __FILE__));
-
-	wp_register_script( 'cbdashboard',plugins_url('media/cbdashboard.compiled.js', __FILE__));
-}
-
-add_action('wp_dashboard_setup', 'chartbeat_add_dashboard_widgets' );
-add_action( 'admin_init', 'chartbeat_plugin_admin_init' );
 
 // Add Column to Posts Manager
 add_filter('manage_posts_columns', 'chartbeat_columns');
@@ -648,7 +410,7 @@ function chartbeat_custom_columns($column_name, $id) {
 			'path' => urlencode( $post_url["path"] ),
 		), '//api.chartbeat.com/live/quickstats/' );
 		?>
-	
+
 		<script type="text/javascript">
 		jQuery.getJSON('<?php echo esc_js( $json_url ); ?>',
 			function(data) {
@@ -669,9 +431,9 @@ function chartbeat_get_static_asset_url( $path = '' ) {
 
 // If admin register settings on page that have been saved
 // if not, add content to wp_head and wp_footer.
-if ( is_admin() ){
+if ( is_admin() ) {
 	add_action( 'admin_init', 'chartbeat_register_settings' );
-}else {
+} else {
 	add_action('wp_head', 'add_chartbeat_head');
 	add_action('wp_footer', 'add_chartbeat_footer');
 }
